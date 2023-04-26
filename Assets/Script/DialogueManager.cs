2b421ede90931696dd4d70f529240cb6f55213b9
Steffen Møgelmose <steffen@mogelmose.org>
2023-04-26 20:58:56 +0200

    Added DialogueSystem

--- Assets/Script/DialogueManager.cs
+++ Assets/Script/DialogueManager.cs
@@ -1,16 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
 
-    public Queue sentences; // FIFO
+    public TextMeshProUGUI textComponent; //
+
+    public string[] lines;
+
+    public float textSpeed;
+
+    private int index;
+
+
+    //public Queue sentences; // FIFO
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComponent.text = string.Empty;
+        StartDialogue();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+
+    }
+
+    IEnumerator TypeLine()
+    {
+        // Type 1 by 1
+        foreach(char c in lines[index].ToCharArray())
+
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+
     }
 
 }
+
+// Inspired by https://www.youtube.com/watch?v=8oTYabhj248&list=PLCDqd_BDUOne2-xb6PtOGhhNA8Qoh1G4V&index=7
