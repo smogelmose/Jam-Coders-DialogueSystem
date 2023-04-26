94150e5be03b1c6018be4785c05756154544affc
Wikkee Jamie Laugesen <72794750+thirteendotexe@users.noreply.github.com>
2023-04-26 18:16:02 +0200

    Menu done

---
+++ Assets/Script/ClickButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ClickButton : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+    [SerializeField] Image _img;
+    [SerializeField] Sprite _default, _pressed;
+    [SerializeField] AudioClip _compressClip, _uncompressedClip;
+    [SerializeField] AudioSource _source;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _img.sprite = _pressed;
+            _source.PlayOneShot(_compressClip);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _img.sprite = _default;
+        _source.PlayOneShot(_uncompressedClip);
+    }
+}
