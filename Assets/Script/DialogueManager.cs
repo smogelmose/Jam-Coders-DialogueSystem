175cdd7db20008e4805e9f6e56f988fafcec1dcf
Steffen Møgelmose <steffen@mogelmose.org>
2023-04-26 19:36:42 +0200

    Added DialogueManager

---
+++ Assets/Script/DialogueManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public Queue sentences; // FIFO
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
