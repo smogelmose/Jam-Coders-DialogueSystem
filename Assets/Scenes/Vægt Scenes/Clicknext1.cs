001024c2d2aa256e8c5b59d906d20e8c7a5cf5fb
Seckit909 <alfred.thorlaksen@gmail.com>
2023-04-27 13:53:52 +0200

    Alfreds gamer comit nr 2

---
+++ Assets/Scenes/Vægt Scenes/Clicknext1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class Clicknext1 : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SceneManager.LoadScene("Scenes/Vægt Scenes/Vægt 2");
+        }
+    }
+}
---
+++ Assets/Scenes/Vægt Scenes/GyroMove.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroMove : MonoBehaviour
+{
+    private Gyroscope gyro;
+    private bool gyroSupported;
+    private Quaternion rotFix;
+
+    void Start()
+    {
+        gyroSupported = SystemInfo.supportsGyroscope;
+
+        if (gyroSupported)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            // Set the rotation fix to take into account the screen orientation of the device
+            if (Screen.orientation == ScreenOrientation.LandscapeLeft)
+                rotFix = new Quaternion(0f, 0f, 1f, 0f);
+
+            if (Screen.orientation == ScreenOrientation.Portrait)
+                rotFix = new Quaternion(0f, 0f, 0f, 1f);
+
+            // Make the image the child of the camera so it moves with it
+            transform.parent = Camera.main.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (gyroSupported)
+        {
+            // Get the gyroscope data and apply the rotation fix
+            Quaternion gyroAttitude = gyro.attitude * rotFix;
+
+            // Apply the rotation to the image
+            transform.localRotation = gyroAttitude;
+        }
+    }
+}
