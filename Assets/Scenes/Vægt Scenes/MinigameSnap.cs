5947bb7ad3205da7b83eb9a410063d15b509c2fc
Freller01 <74055835+Seckit909@users.noreply.github.com>
2023-04-27 21:35:00 +0200

    Gamermode Minigame ting

---
+++ Assets/Scenes/Vægt Scenes/MinigameSnap.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MinigameSnap : MonoBehaviour
+{
+    public RectTransform SnapOnCorrect;
+    public float SnapPossition = 5f;
+    private RectTransform TransformerMode;
+
+    private void Start()
+    {
+        TransformerMode = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        // Calculate the distance between the current position and the correct position
+        float distance = Vector2.Distance(TransformerMode.anchoredPosition, SnapOnCorrect.anchoredPosition);
+
+        // If the distance is within the snap distance, snap the image to the correct position
+        if (distance <= SnapPossition)
+        {
+            TransformerMode.anchoredPosition = SnapOnCorrect.anchoredPosition;
+        }
+    }
+}
