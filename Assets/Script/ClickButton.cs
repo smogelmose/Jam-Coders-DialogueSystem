7c2eaed3590cc097101aeb7b007de132a5354f68
Freller01 <74055835+Seckit909@users.noreply.github.com>
2023-04-28 09:12:16 +0200

    Nu uden bugs

--- Assets/Script/ClickButton.cs
+++ Assets/Script/ClickButton.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEditor.Build.Content;
 using JetBrains.Annotations;
 
 public class ClickButton : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
